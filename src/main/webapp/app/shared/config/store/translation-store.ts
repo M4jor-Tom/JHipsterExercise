bc394d354ca69f8cacdc7ce534886b2fedd70198
Tom <tom-vautray@hotmail.fr>
2021-11-25 10:34:05 +0100

    Initial version of JHipsterExercise generated by generator-jhipster@7.3.1

---
+++ src/main/webapp/app/shared/config/store/translation-store.ts
@@ -0,0 +1,25 @@
+import { Module } from 'vuex';
+
+export const translationStore: Module<any, any> = {
+  state: {
+    currentLanguage: localStorage.getItem('currentLanguage') || 'en',
+    languages: {
+      en: { name: 'English' },
+      fr: { name: 'Français' },
+      de: { name: 'Deutsch' },
+      it: { name: 'Italiano' },
+      es: { name: 'Español' },
+      // jhipster-needle-i18n-language-key-pipe - JHipster will add/remove languages in this object
+    },
+  },
+  getters: {
+    currentLanguage: state => state.currentLanguage,
+    languages: state => state.languages,
+  },
+  mutations: {
+    currentLanguage(state, newLanguage) {
+      state.currentLanguage = newLanguage;
+      localStorage.setItem('currentLanguage', newLanguage);
+    },
+  },
+};
