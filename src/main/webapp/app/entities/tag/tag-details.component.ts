ed21ff31d5d33fdf03c2b3a9cf9ae5177c5e3c84
Abdelkhalek El Omari <abdelkhalek.elomari@soprasteria.com>
2021-11-26 10:27:30 +0100

    Adaptation de la sécurité aux nouvelles entitées : tag & sub-family.

--- src/main/webapp/app/entities/tag/tag-details.component.ts
+++ src/main/webapp/app/entities/tag/tag-details.component.ts
@@ -4,12 +4,19 @@ import { ITag } from '@/shared/model/tag.model';
 import TagService from './tag.service';
 import AlertService from '@/shared/alert/alert.service';
 
+import AccountService from '@/account/account.service';
+import LoginService from '@/account/login.service';
+
 @Component
 export default class TagDetails extends Vue {
   @Inject('tagService') private tagService: () => TagService;
   @Inject('alertService') private alertService: () => AlertService;
 
   public tag: ITag = {};
+  @Inject('accountService') private accountService: () => AccountService;
+  @Inject('loginService')
+  private loginService: () => LoginService;
+  private hasAnyAuthorityValue = false;
 
   beforeRouteEnter(to, from, next) {
     next(vm => {
@@ -32,5 +39,18 @@ export default class TagDetails extends Vue {
 
   public previousState() {
     this.$router.go(-1);
+  }
+  
+  public get authenticated(): boolean {
+    return this.$store.getters.authenticated;
+  }
+
+  public hasAnyAuthority(authorities: any): boolean {
+    this.accountService()
+      .hasAnyAuthorityAndCheckAuth(authorities)
+      .then(value => {
+        this.hasAnyAuthorityValue = value;
+      });
+    return this.hasAnyAuthorityValue;
   }
 }
