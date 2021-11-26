3ecfb9e40ff18919c20083511045a0ec2f980a1a
Teddy-Jacono <teddy.jacono@gmail.com>
2021-11-26 16:15:56 +0100

    Add Tri Tags
    Ajout du tri sur Tags

--- src/main/java/com/mycompany/myapp/web/rest/TagResource.java
+++ src/main/java/com/mycompany/myapp/web/rest/TagResource.java
@@ -13,10 +13,15 @@ import javax.validation.constraints.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import tech.jhipster.web.util.HeaderUtil;
+import tech.jhipster.web.util.PaginationUtil;
 import tech.jhipster.web.util.ResponseUtil;
 
 /**
@@ -141,9 +146,11 @@ public class TagResource {
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of tags in body.
      */
     @GetMapping("/tags")
-    public List<Tag> getAllTags() {
-        log.debug("REST request to get all Tags");
-        return tagRepository.findAll();
+    public ResponseEntity<List<Tag>> getAllFamilies(Pageable pageable) {
+        log.debug("REST request to get all Families");
+        Page<Tag> page = tagRepository.findAll(pageable);
+        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
+        return ResponseEntity.ok().headers(headers).body(page.getContent());
     }
 
     /**
--- src/main/webapp/app/entities/tag/tag.component.ts
+++ src/main/webapp/app/entities/tag/tag.component.ts
@@ -27,6 +27,9 @@ export default class Tag extends Vue {
 
   public isFetching = false;
 
+  public propOrder = 'id';
+  public reverse = false;
+
   public mounted(): void {
     this.retrieveAllTags();
   }
@@ -38,7 +41,9 @@ export default class Tag extends Vue {
   public retrieveAllTags(): void {
     this.isFetching = true;
     this.tagService()
-      .retrieve()
+      .retrieve({
+        sort: this.sort(),
+      })
       .then(
         res => {
           this.tags = res.data;
@@ -98,5 +103,23 @@ export default class Tag extends Vue {
         this.hasAnyAuthorityValue = value;
       });
     return this.hasAnyAuthorityValue;
+  }
+
+  public changeOrder(propOrder: string): void {
+    this.propOrder = propOrder;
+    this.reverse = !this.reverse;
+    this.transition();
+  }
+
+  public transition(): void {
+    this.retrieveAllTags();
+  }
+
+  public sort(): any {
+    const result = [this.propOrder + ',' + (this.reverse ? 'desc' : 'asc')];
+    if (this.propOrder !== 'id') {
+      result.push('id');
+    }
+    return result;
   }
 }
